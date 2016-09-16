70b6f840e4dd1b29dca432c03987611e0e9d76df
neoyoun <453400438@qq.com>
2016-09-17 02:52:14 +0800

    需要完善动画衔接问题

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -8,12 +8,34 @@ let CarouselComponent = function (userConfig) {
 		const defaultConfig = {
 			interval : 5000,
 			direction : 'left',
-			delay : 0
+			pause : 'hover'
 		}
-		let config = Object.assign(defaultConfig,userConfig);
 		let childs = self.childNodes;
-		let listBox,prevBtn,nextBtn;
+		let config,listBox,prevBtn,nextBtn;
 
+		if( userConfig !== undefined ){
+			if( typeof userConfig === Object ){
+				config = Object.assign(defaultConfig,userConfig);
+			}else if ( typeof userConfig === 'string' ){
+				switch(userConfig) {
+					case 'hover':
+						pauseAutoSlide();
+						break;
+					case 'prev':
+						slideToPrev();
+						break;
+					case 'next':
+						slideToNext();
+						break;
+					default :
+						break;
+				}
+			}
+		}else {
+			config = defaultConfig;
+		}
+		
+// find slide items's wraper
 		for (let i = 0; i < childs.length; i++) {
 			if(childs[i].nodeType == 1 && childs[i].getAttribute('role')){
 				if( childs[i].getAttribute('role') == 'list-box'){
@@ -25,22 +47,75 @@ let CarouselComponent = function (userConfig) {
 			console.error('slide item must be wraped');
 			return false;
 		}
-		for (let i = 0; i < slideItems.length; i++) {
+
+initialDOM(config);
+
+ function initialDOM(config) {
+  /*
+	 *初始化节点，包括样式和DOM结构的创建。
+	 *防止滚动到最后一个节点的时候出现空白，在头一个滚动的
+	 *的节点复制到队列最后。
+	 *因此样式上盒子的宽度比滚动项多一个。高度在css中设置一致。
+	 *滚动项的高度和宽度保持和祖先元素一致；
+	 *最后调用动画方法
+	 */
+	for (let i = 0; i < slideItems.length; i++) {
 			slideItems[i].style.width = self.offsetWidth + 'px';
 			slideItems[i].style.height = self.offsetHeight + 'px';
 		}
-		listBox.style.width = (slideItems[0].offsetWidth * (slideItems.length+1)) +'px';
-		let copyItem = listBox.firstElementNode.cloneNode();
+	listBox.style.width = (slideItems[0].offsetWidth * (slideItems.length+1)) +'px';
+	
+	if( config.direction == 'left' ){
+		let copyItem = listBox.firstElementChild.cloneNode(true);
+		listBox.appendChild(copyItem)
+	}else {
+		let copyItem = listBox.lastElementChild.cloneNode(true);
+		listBox.insertBefore(copyItem , listBox.firstElementNode);
+	}
+	return startSlide(config);
+}
 
-		if( self.classList.contains('slide')){
-			startAutoSlide(config)
-		}
+function startSlide(config) {
+	let interval = config.interval;
+	let direction = config.direction;
+	let step = self.offsetWidth;
+	let curSlideIndex = 0;
 
-		function startAutoSlide(config) {
-			let interval = config.interval;
-			let direction = config.direction;
-		}
+	switch(direction) {
+		case 'left':
+			slideToLeft();
+			break;
+		case 'right':
+			slideToRight();
+			break;
+		default :
+			break;
+	}
 
+	function slideToLeft() {
+		let timer = setInterval(function () {
+			if( curSlideIndex == slideItems.length ){
+				curSlidePos = curSlideIndex * step;
+				curSlideIndex = 0;
+				listBox.style.transitionDuration = '.3s';
+				listBox.style.transform = 'translate3d(-'+curSlidePos + 'px, 0 ,0)';
+				setTimeout(function () {
+					listBox.style.transitionDuration = '0s';
+					listBox.style.transform = 'translate3d(0, 0 ,0)';
+				}, 300)
+				
+			}else {
+				curSlideIndex ++ ;
+				curSlidePos = curSlideIndex * step;
+				listBox.style.transitionDuration = '.3s';
+				listBox.style.transform = 'translate3d(-'+curSlidePos + 'px, 0 ,0)';
+			}
+			
+			}, interval)
+	}
+	
+}
+		
 
 }
 Object.defineProperty(HTMLElement.prototype,'slide',{
